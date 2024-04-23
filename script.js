d824392262b1e08ea2232f2ba6fba36908042be1
JMcKaay <162724220+JMcKaay@users.noreply.github.com>
2024-04-23 07:44:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleSubmit(event) {
+    event.preventDefault ();
+    
+    var name = document.getElementById('name').value;
+    
+    alert("Hey " + name + ", we will contact you shortly.");
+}
+
+           
